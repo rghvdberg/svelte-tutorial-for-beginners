7f82d86afe3a84ac93dbf4d842b53462046cabb0
Rob van den Berg <rghvdberg@gmail.com>
2023-04-16 15:52:45 +0200

    move file

--- images/readme.svelte
+++ images/readme.svelte
@@ -5,3 +5,15 @@
 <!-- App   -->
 <!-- svelte-ignore a11y-click-events-have-key-events missing-declaration -->
 <myComponent on:click|once={myFunction} />
+
+<myComponent>
+  <div slot="namedSlot">
+    <p>Named Slot</p>
+    <p />
+  </div>
+</myComponent>
+
+<div class="modal">
+  <slot />
+  <slot name="namedSlot" />
+</div>
