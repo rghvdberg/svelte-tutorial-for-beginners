bd1014d98ffb151e586c2ce1d1656cc593280d79
Rob van den Berg <rghvdberg@gmail.com>
2023-04-19 21:34:09 +0200

    #20 @ 9:35 https://youtu.be/U8cM_jhmDxA?t=575

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,11 +1,16 @@
+<!-- <!- @ 9:35 https://youtu.be/U8cM_jhmDxA?t=575  -->
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  // tabs
+  import Tabs from "./components/shared/Tabs.svelte";
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
 </script>
 
 <Header />
 <main>
-  <!-- content here -->
+  <Tabs {items} {activeItem} />
 </main>
 <Footer />
 
