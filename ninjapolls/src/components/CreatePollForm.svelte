cc3f91faff65fbcb81d83423eb9e7f31953d3817
Rob van den Berg <rghvdberg@gmail.com>
2023-04-27 21:02:44 +0200

    move `shared`

--- ninjapolls/src/components/CreatePollForm.svelte
+++ ninjapolls/src/components/CreatePollForm.svelte
@@ -1,6 +1,6 @@
 <script>
   import { createEventDispatcher } from "svelte";
-  import Button from "./shared/Button.svelte";
+  import Button from "../shared/Button.svelte";
 
   let dispatch = createEventDispatcher();
 
